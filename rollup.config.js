be3ac4c37fcf7c32c71b5050d18cfde4f2f0f6c6
Jason Johnston <jason+github@lojjic.com>
2020-05-24 22:06:54 -0600

    Fix rollup build breakage due to terser plugin upgrade

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,10 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
-
-const base = {
+export default {
   input: 'src/index.js',
-  output: {
-    format: 'iife',
-    file: 'dist/aframe-troika-text.js',
-    globals: {
-      three: 'THREE',
-      aframe: 'AFRAME'
-    }
-  },
-  external: ['three', 'aframe']
-}
-
-export default [
-  // Full:
-  {
-    input: 'src/index.js',
-    output: {
+  output: [
+    {
       format: 'iife',
       file: 'dist/aframe-troika-text.js',
       globals: {
@@ -27,28 +12,21 @@ export default [
         aframe: 'AFRAME'
       }
     },
-    external: ['three', 'aframe'],
-    plugins: [
-      nodeResolve()
-    ]
-  },
-
-  // Minified:
-  {
-    input: 'src/index.js',
-    output: {
+    {
       format: 'iife',
       file: 'dist/aframe-troika-text.min.js',
       globals: {
         three: 'THREE',
         aframe: 'AFRAME'
       },
-      sourcemap: true
-    },
-    external: ['three', 'aframe'],
-    plugins: [
-      nodeResolve(),
-      terser()
-    ]
-  }
-]
+      sourcemap: true,
+      plugins: [
+        terser()
+      ]
+    }
+  ],
+  external: ['three', 'aframe'],
+  plugins: [
+    nodeResolve()
+  ]
+}
