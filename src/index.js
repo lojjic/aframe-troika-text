fdfc50ab5b0f4aea2e5afadbf76c2ee064cd98e7
Jason Johnston <jason+github@lojjic.com>
2020-10-20 21:00:08 -0600

    Update to troika-three-text 0.34.x

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import * as THREE from 'three'
+
+// Polyfill Three's rename of Math->MathUtils after the super-three fork
+(function(ThreedleDum) {
+  if (!ThreedleDum.MathUtils) {
+    ThreedleDum.MathUtils = ThreedleDum.Math
+  }
+})(THREE)
+
 import './aframe-troika-text-component.js'
 import './aframe-troika-text-primitive.js'
